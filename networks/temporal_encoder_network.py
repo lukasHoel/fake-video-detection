optical-flow calculation in solver and minor bug fixes

networks/temporal_encoder_network.py
@@ -105,50 +105,31 @@ class TemporalEncoder(nn.Module):
         )
 
     def forward(self, x):
+        # by convention (see also solver) this is how the input is delivered depending on optical flow available
+        if self.useOpticalFlow:
+            images = x["image"]
+            warps = x["warp"]
+        else:
+            images = x
+
         image_features = [] # image features of every video frame in x independently
         flow_features = [] # if optical flow shall be calculated: save it's image features from warped image in this list
-        sequence_features = [] # features of every self.delta_t*2 + 1 image features
+        sequence_features = [] # features of every self.delta_t*2 + 1 image (and flow) features
         predictions = [] # predictions per sequence_feature
 
         #print("Start forward pass")
         # 1.a for every video frame in sequence x: calculate features with self.feature_extractor
         for i in range(self.num_input_images):
-            x_i = x[:, :, i, :, :, :]
+            x_i = images[:, :, i, :, :, :]
             x_i = x_i.squeeze() # remove dim=(1,2)
             y_i = self.feature_extractor(x_i)
             image_features.append(y_i)
         #print("Image feature extraction finished")
 
-        # 1.b if optical flow is enabled: calculate optical flow to center of sequence from each image
-        # and save image features of warped image
+        # 1.b if optical flow is enabled: calculate image features of warped image in each sequence
         if self.useOpticalFlow:
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-            center_images = x[:, :, self.num_input_images//2, :, :, :]
-            center_images = center_images.squeeze() # remove dim=(1,2)
-
-            warp_images = torch.zeros_like(x)
-
-            for b in range(center_images.shape[0]):
-                center_image = center_images[b].data.cpu().numpy()
-                center_image = center_image.transpose((1,2,0))
-
-                for i in range(self.num_input_images):
-                    if i == self.num_input_images//2:
-                        pass
-                        # TODO do we really need to calculate this flow?
-                        # TODO I guess not... how to make sure that this can be skipped in step 2 when selecting from flow_features?
-                    x_i = x[b, :, i, :, :, :]
-                    x_i = x_i.squeeze() # remove dim=(1,2)
-                    x_i = x_i.data.cpu().numpy()
-                    x_i = x_i.transpose((1,2,0))
-
-                    warp = warp_from_images(x_i, center_image)
-                    warp = warp.transpose((2,0,1))
-                    warp = torch.from_numpy(warp).float().to(device)
-                    warp_images[b, :, i] = warp
-            #print("Flow calculation finished")
             for i in range(self.num_input_images):
-                x_i = warp_images[:, :, i, :, :, :]
+                x_i = warps[:, :, i, :, :, :]
                 x_i = x_i.squeeze()  # remove dim=(1,2)
                 y_i = self.feature_extractor(x_i)
                 flow_features.append(y_i)

training/sequence_dataloader.py
@@ -1,3 +1,5 @@
+from utils.warp_image_farneback import warp_from_images
+
 from skimage import io
 from torch.utils import data
 import torch
@@ -8,7 +10,7 @@ from tqdm.auto import tqdm
 
 
 class FaceForensicsVideosDataset(data.Dataset):
-    def __init__(self, directories, num_frames, generate_coupled=False, transform=None, max_number_videos_per_directory=-1):
+    def __init__(self, directories, num_frames, generate_coupled=False, transform=None, max_number_videos_per_directory=-1, calculateOpticalFlow=True, verbose=False):
         """
         Args:
         directories: List of paths where the images for the dataset are
@@ -23,11 +25,16 @@ class FaceForensicsVideosDataset(data.Dataset):
         self.num_videos = 0
         self.num_frames=num_frames
         self.max_number_videos_per_directory = max_number_videos_per_directory
-
+        self.calculateOpticalFlow = calculateOpticalFlow
+        self.verbose = verbose
         self.frame_dir = {}
         counter = 0
+        number_directories = 0
         for path in directories:
             # Get all folders with videos in the directory at path
+            number_directories += 1
+            print("Loading directory {}/{}: {}".format(number_directories, len(directories), path))
+
             number_videos_for_directory = 0
             video_folders = [f for f in os.listdir(path) if not os.path.isfile(os.path.join(path, f))]
             for f in tqdm(video_folders):
@@ -65,18 +72,14 @@ class FaceForensicsVideosDataset(data.Dataset):
                                    os.path.isfile(os.path.join(s, f)) and f.endswith(".png")]
                     image_names = image_names[0:self.num_frames]
 
-                    #this_sample = []
-
-                    # Read all images into an image list and stack to 1 numpy matrix
+                    # Read all image file names into a list for lazy-loading upon first get_item request
                     image_paths = []
                     for name in image_names:
                         image_paths.append(s + "/" + name)
-                        #this_sample.append(io.imread(s + "/" + name))
-                    #image_matrix = np.stack(this_sample)
 
                     key = counter
                     counter += 1
-                    self.frame_dir[key] = (False, image_paths, label)
+                    self.frame_dir[key] = (False, image_paths, label, None)
         self.dataset_length = counter
 
     # number of samples in the dataset
@@ -133,20 +136,40 @@ class FaceForensicsVideosDataset(data.Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        is_loaded, sequence, label = self.frame_dir[idx]
+        is_loaded, sequence, label, warp = self.frame_dir[idx]
 
+        # lazy-load sequence when it is requested for the first time
         if not is_loaded:
-            print("Sequence {} not loaded, will load now".format(idx))
             this_sample = []
-            for name in tqdm(sequence):
-                this_sample.append(io.imread(name))
+            if self.verbose:
+                print("loading new sequence into cache")
+                for name in tqdm(sequence):
+                    this_sample.append(io.imread(name))
+            else:
+                for name in sequence: # iterate without tqdm(...)
+                    this_sample.append(io.imread(name))
             sequence = np.stack(this_sample)
-            self.frame_dir[idx] = (True, sequence, label)
-            print("Loaded sequence {} into global dataset directory".format(idx))
 
+            if self.calculateOpticalFlow:
+                warp = []
+                n = sequence.shape[0]
+                center_image = sequence[n//2]
+                if self.verbose:
+                    print("calculating and loading optical-flow warp into cache")
+                    for i in tqdm(range(n)):
+                        warp.append(warp_from_images(sequence[i], center_image))
+                else:
+                    for i in range(n): # iterate without tqdm(...)
+                        warp.append(warp_from_images(sequence[i], center_image))
+                warp = np.stack(warp)
+
+            self.frame_dir[idx] = (True, sequence, label, warp)
+
+        # todo why do we need these two lines? in the models, we squeeze the extra dimension everytime?
         samples = np.stack([sequence])
-        #print(samples.shape)
-        sample = {"image": samples, "label": label}
+        warps = np.stack([warp])
+
+        sample = {"image": samples, "label": label, "warp": warps}
 
         if self.transform:
             sample = self.transform(sample)
@@ -158,21 +181,24 @@ class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        samples, labels = sample["image"], sample["label"]
+        samples, labels, warps = sample["image"], sample["label"], sample["warp"]
 
         # swap color axis because
         # numpy image: num_frames x H x W x C
         # torch image: num_frames x C X H X W
         samples = torch.from_numpy(samples.transpose((0, 1, 4, 2, 3)))
+        warps = torch.from_numpy(warps.transpose((0, 1, 4, 2, 3)))
         labels = torch.tensor(labels)
         return {"image": samples.float(),
-                "label"   : torch.tensor(labels).long()}
+                "label": labels.long(),
+                "warp": warps.float()}
 
 
 def my_collate(batch):
     data = np.concatenate([b["image"] for b in batch], axis=0)
     targets = [b["label"] for b in batch]
-    sample = {"image": data, "label": targets}
+    warp = [b["warp"] for b in batch]
+    sample = {"image": data, "label": targets, "warp": warp}
     return sample
 
 

training/solver.py
@@ -11,6 +11,13 @@ def wrap_data(xb, yb, device):
 
     return xb, yb
 
+def wrap_data(xb, yb, warpb, device):
+    xb, yb, warpb = Variable(xb), Variable(yb), Variable(warpb)
+    if str(device) != 'cpu':
+        xb, yb, warpb = xb.cuda(), yb.cuda(), warpb.cuda()
+
+    return xb, yb, warpb
+
 class Solver(object):
     default_adam_args = {"lr": 1e-4,
                          "betas": (0.9, 0.999),
@@ -49,6 +56,24 @@ class Solver(object):
         self.val_acc_history = []
         self.val_loss_history = []
 
+    def forward_pass(self, model, sample, device):
+        xb = sample["image"]
+        yb = sample["label"]
+        warpb = sample.get("warp") # use get function here to ensure None is returned if key is not set instead of KeyError: https://stackoverflow.com/questions/6130768/return-none-if-dictionary-key-is-not-available
+
+        if warpb is not None:
+            xb, yb, warpb = wrap_data(xb, yb, warpb, device)
+            x = {"image": xb, "warp": warpb} # by convention this is the input to the model when optical_flow is available
+            scores = model(x)
+        else:
+            xb, yb = wrap_data(xb, yb, device)
+            scores = model(xb)
+
+        loss = self.loss_func(scores, yb)
+        acc = self.accuracy(scores, yb)
+
+        return loss, acc
+
     def test(self, model, test_loader, test_prefix='/', log_nth=0):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model.to(device)
@@ -63,17 +88,9 @@ class Solver(object):
             test_losses = []
             test_accs = []
             for i, sample in enumerate(test_loader):
-                xb = sample["image"]
-                yb = sample["label"]
-                xb, yb = wrap_data(xb, yb, device)
-
-                # FORWARD PASS --> Loss calculation
-                scores = model(xb)
-                loss = self.loss_func(scores, yb)
+                loss, test_acc = self.forward_pass(model, sample, device)
                 loss = loss.data.cpu().numpy()
                 test_losses.append(loss)
-
-                test_acc = self.accuracy(scores, yb)
                 test_accs.append(test_acc)
 
                 self.writer.add_scalar('Test/' + test_prefix + 'Batch/Loss', loss, i)
@@ -118,13 +135,8 @@ class Solver(object):
             train_losses = []
             train_accs = []
             for i, sample in enumerate(train_loader):  # for every minibatch in training set
-                xb = sample["image"]
-                yb = sample["label"]
-                xb, yb = wrap_data(xb, yb, device)
-
-                # FORWARD PASS --> Loss calculation
-                scores = model(xb)
-                train_loss = self.loss_func(scores, yb)
+                # FORWARD PASS --> Loss + acc calculation
+                train_loss, train_acc = self.forward_pass(model, sample, device)
 
                 # BACKWARD PASS --> Gradient-Descent update
                 train_loss.backward()
@@ -134,8 +146,6 @@ class Solver(object):
                 # LOGGING of loss and accuracy
                 train_loss = train_loss.data.cpu().numpy()
                 train_losses.append(train_loss)
-
-                train_acc = self.accuracy(scores, yb)
                 train_accs.append(train_acc)
 
                 self.writer.add_scalar('Batch/Loss/Train', train_loss, i + epoch * iter_per_epoch)
@@ -169,17 +179,10 @@ class Solver(object):
                 val_losses = []
                 val_accs = []
                 for i, sample in enumerate(val_loader):
-                    xb = sample["image"]
-                    yb = sample["label"]
-                    xb, yb = wrap_data(xb, yb, device)
-
-                    # FORWARD PASS --> Loss calculation
-                    scores = model(xb)
-                    val_loss = self.loss_func(scores, yb)
+                    # FORWARD PASS --> Loss + acc calculation
+                    val_loss, val_acc = self.forward_pass(model, sample, device)
                     val_loss = val_loss.data.cpu().numpy()
                     val_losses.append(val_loss)
-
-                    val_acc = self.accuracy(scores, yb)
                     val_accs.append(val_acc)
 
                     self.writer.add_scalar('Batch/Loss/Val', val_loss, i + epoch*len(val_loader))