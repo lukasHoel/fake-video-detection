30acca5861101853594dc54d5aa458ebb581a4f2
Anna Mittermair <anna.mittermair@tum.de>
2020-01-12 01:28:32 +0100

    Fix train test split in dataloader?

--- training/sequence_dataloader.py
+++ training/sequence_dataloader.py
@@ -1,5 +1,4 @@
-from utils.warp_image_farneback import warp_from_images
-
+#from utils.warp_image_farneback import warp_from_images
 from skimage import io
 from torch.utils import data
 import torch
@@ -7,6 +6,7 @@ import os
 import numpy as np
 import math
 from tqdm.auto import tqdm
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
 class FaceForensicsVideosDataset(data.Dataset):
@@ -93,10 +93,9 @@ class FaceForensicsVideosDataset(data.Dataset):
     # returns two lists of indices, one for training and one for test cases
     def get_train_val_lists(self, part_train, part_val):
         label_list = [[0, 0]]
-        last_label = (self.frame_dir[0][1] == -1)
-
+        last_label = self.frame_dir[0][2]
         for key in self.frame_dir:
-            if (self.frame_dir[key][1] == -1) == last_label:
+            if self.frame_dir[key][2] == last_label:
                 label_list[-1][1] += 1
             else:
                 label_list.append([key, 0])
@@ -187,7 +186,10 @@ class ToTensor(object):
         # numpy image: num_frames x H x W x C
         # torch image: num_frames x C X H X W
         samples = torch.from_numpy(samples.transpose((0, 1, 4, 2, 3)))
-        warps = torch.from_numpy(warps.transpose((0, 1, 4, 2, 3)))
+        if len(warps.shape) == 5:
+            warps = torch.from_numpy(warps.transpose((0, 1, 4, 2, 3)))
+        else:
+            warps = torch.Tensor(0)
         labels = torch.tensor(labels)
         return {"image": samples.float(),
                 "label": labels.long(),
@@ -203,19 +205,32 @@ def my_collate(batch):
 
 
 if __name__ == '__main__':
+    from torch.utils.data.sampler import SubsetRandomSampler
+
     # test /example
-    d = ["/home/anna/Desktop/Uni/WiSe19/DL4CV/adl4cv/data/FaceForensics/manipulated_sequences/Deepfakes/c40/sequences_299x299_5seq@10frames_skip_5_uniform"]
-    test_dataset = FaceForensicsVideosDataset(d, generate_coupled=False, num_frames=5, transform=ToTensor())
+    d = ["/home/anna/Desktop/Uni/WiSe19/DL4CV/adl4cv/data/FaceForensics/original_sequences/youtube/c40/sequences_299x299_5seq@10frames_skip_5_uniform",
+        "/home/anna/Desktop/Uni/WiSe19/DL4CV/adl4cv/data/FaceForensics/manipulated_sequences/Deepfakes/c40/sequences_299x299_5seq@10frames_skip_5_uniform"]
+
+
+    test_dataset = FaceForensicsVideosDataset(d, generate_coupled=False, num_frames=10, calculateOpticalFlow=False, transform=ToTensor())
     print(test_dataset.__len__())
-    train_list, val_list = test_dataset.get_train_val_lists(0.9, 0.01)
+    train_list, val_list = test_dataset.get_train_val_lists(0.2, 0.8)
     print(len(train_list), len(val_list))
     dataset_loader = torch.utils.data.DataLoader(test_dataset,
                                                  batch_size=4, shuffle=True,
                                                  collate_fn=my_collate,  # use custom collate function here
                                                  pin_memory=True)
 
-    for i, sample in enumerate(dataset_loader):
-        print("->", sample["image"].shape)
-        print(sample["label"])
-        pass
+    train_sampler = SubsetRandomSampler(train_list)
+    valid_sampler = SubsetRandomSampler(val_list)
+
+    train_loader = torch.utils.data.DataLoader(test_dataset, batch_size=2,
+                                               sampler=train_sampler)
+    validation_loader = torch.utils.data.DataLoader(test_dataset, batch_size=2,
+                                                    sampler=valid_sampler)
+
+    # for v in list(validation_loader):
+    #     print(v["label"])
+        
+
 
